5da22922de13024ea3a63f110bec7ea5cde50a80
kenmutesh <kenmutesh901@gmail.com>
2024-02-23 09:56:37 +0300

    actions file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
 <section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
         <div class="contact-info d-flex align-items-center">
-            <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
+            <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:info@gospelcommunityfellowship.com">info@gospelcommunityfellowship.com</a></i>
             <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
         </div>
         <div class="social-links d-none d-md-flex align-items-center">
@@ -67,9 +67,6 @@
 <script src="{{ asset('assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
 <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
 
-<!-- Template Main JS File -->
 <script src="{{ asset('assets/js/main.js') }}"></script>
-
-</body>
 
 </html>
